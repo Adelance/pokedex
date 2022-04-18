19734e1dda2e0047c2cbf5a3a7a21b197634f88b
unknown <blobos228@gmail.com>
2022-04-18 17:13:51 +0200

    Last touches. Added save of darkmode on localStorege

--- src/components/layout/DarkModeBtn.tsx
+++ src/components/layout/DarkModeBtn.tsx
@@ -1,22 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const DarkModeBtn = () => {
+    const [darkModeIcon, setDarkModeIcon] = useState(false);
 
-    const [darkMode, setDarkMode] = useState(false);
+    useEffect(() => {
+        if (localStorage.getItem('theme')) {
+            document.body.classList.toggle('dark');
+            setDarkModeIcon(true)
+        }
+    }, [])
 
     const onClick = () => {
-        setDarkMode(!darkMode)
+        if (localStorage.getItem('theme')) localStorage.removeItem('theme')
+        else localStorage.setItem('theme', 'darkOn')
         document.body.classList.toggle('dark');
+        setDarkModeIcon(!darkModeIcon)
     }
-    
+
     return (
         <div className='fixed-action-btn'>
             <a className="btn-floating btn-large" onClick={onClick}>
-             {darkMode ? (<i className='material-icons'>dark_mode</i>
-             ) : (
-                <i className='material-icons'>light_mode</i>
-             )}
-                
+                {darkModeIcon ? (<i className='material-icons'>dark_mode</i>
+                ) : (
+                    <i className='material-icons'>light_mode</i>
+                )}
+
             </a>
         </div>
     )
--- src/components/pokemons/PokemonItem.tsx
+++ src/components/pokemons/PokemonItem.tsx
@@ -1,33 +1,33 @@
 import { useDispatch } from "react-redux";
 import { setCurrent } from "../../actions/pokemonActions";
 
-const PokemonItem = ({pokemon}:any) => {
+const PokemonItem = ({ pokemon }: any) => {
   const dispatch = useDispatch();
   const capitalize = (str: string) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
-  
+
   return (
     <div className='collection-item row'>
-      
+
       <a href='#view-pokemon-modal' className="modal-trigger texting" onClick={() => setCurrent(pokemon)(dispatch)}>
         <div className="column sprite">
-            <img src={pokemon.sprites.other.home.front_default} width="150" height="150" alt='PokeDex'/>
+          <img src={pokemon.sprites.other.home.front_default} width="150" height="150" alt='PokeDex' />
         </div>
-        <span className="">
+        <span>
           <h6>Name:</h6>
-          <h3>{capitalize(pokemon.name)}</h3> 
-        </span>          
-          <span>Types: </span> 
-            {             
-            pokemon.types.map((element: any, index: any) => 
-              <span className="" key={index}>{capitalize(element.type.name)} </span>
-              )
-            }
-          
+          <h3>{capitalize(pokemon.name)}</h3>
+        </span>
+        <span>Types: </span>
+        {
+          pokemon.types.map((element: any, index: any) =>
+            <span className="" key={index}>{capitalize(element.type.name)} </span>
+          )
+        }
+
       </a>
     </div>
- 
+
   )
 }
 
