a4b4828a36836bc8a7b6469d936fec48fe08ac53
unknown <blobos228@gmail.com>
2022-04-26 21:31:52 +0200

    fixed pokemon item

--- src/components/pokemons/PokemonItem.tsx
+++ src/components/pokemons/PokemonItem.tsx
@@ -8,30 +8,36 @@ const PokemonItem = ({ pokemon }: any) => {
     };
 
     return (
-        <div className="collection-item row">
+        <div className="collection-item">
             <a
                 href="#view-pokemon-modal"
                 className="modal-trigger texting"
                 onClick={() => setCurrent(pokemon)(dispatch)}
             >
-                <div className="column sprite">
-                    <img
-                        src={pokemon.sprites.other.home.front_default}
-                        width="150"
-                        height="150"
-                        alt="PokeDex"
-                    />
+                <div className="flex-container">
+                    <div>
+                        <img
+                            src={pokemon.sprites.other.home.front_default}
+                            width="150"
+                            height="150"
+                            alt="PokeDex"
+                        />
+                    </div>
+                    <div>
+                        Name:
+                        <div>
+                            <h3>{capitalize(pokemon.name)}</h3>
+                        </div>
+                    </div>
+                    <div>
+                        Types:
+                        {pokemon.types.map((element: any, index: any) => (
+                            <div key={index}>
+                                {capitalize(element.type.name)}
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                <span>
-                    <h6>Name:</h6>
-                    <h3>{capitalize(pokemon.name)}</h3>
-                </span>
-                <span>Types: </span>
-                {pokemon.types.map((element: any, index: any) => (
-                    <span className="" key={index}>
-                        {capitalize(element.type.name)}{' '}
-                    </span>
-                ))}
             </a>
         </div>
     );
